2731a0e3f508bf4d8d0ae23b8e255852765fc4f4
Kalia Hayes <kaliahayes@yahoo.com>
2018-12-02 17:03:43 -0600

    New Website
    Updated files

---
+++ php/twitter/config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Your Twitter App Info
+ * How to create Twitter API key you can read here (or use google):  - http://www.gabfirethemes.com/create-twitter-api-key/
+ */
+
+ // User Name
+ define('USER_NAME', 'kaliahayes');
+
+// Consumer Key
+define('CONSUMER_KEY', '2JcuI4Gw2XnKcSMnqjz4kbVJl');
+define('CONSUMER_SECRET', 'UOW87PVQsI6Gnt5jUpsPcsvmVrv9VcfzRTZns6v8ArPKuOyosB');
+
+// User Access Token
+define('ACCESS_TOKEN', '923209841962872833-6XkYi4FkPVuzTjSpFgU03MIdNc8iOKI');
+define('ACCESS_SECRET', '70S8VS3cgyyDN5ePU4GFxzdofLAWS0T2SabBxmJ4ZnvSA');
+
+// Cache Settings
+define('CACHE_ENABLED', true);
+define('CACHE_LIFETIME', 3600); // in seconds
+define('HASH_SALT', md5(dirname(__FILE__)));
